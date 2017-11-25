3f74500aaa2d2f9aa138b9fc744a64aefc7bb7c0
Paal Thorstensen <paal@computer.org>
2017-11-24 20:44:30 -0800

    Store CLI in Google bucket

--- build-tools/build-client.sh
+++ build-tools/build-client.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 go install cmd/kless/kless.go
+
+if [[ ! -z "$KLESS_UPLOAD_CLI" ]]; then
+  gsutil cp $GOBIN/kless gs://klesscli/$BUILD_ID/bin/linux/kless
+  gsutil acl ch -u AllUsers:R gs://klesscli/$BUILD_ID/bin/linux/kless
+fi
